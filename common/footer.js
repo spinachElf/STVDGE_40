1c0e4f5de10d20e15eb2806f0ef9e93e563e3589
Nayun-Kim17 <80013175+Nayun-Kim17@users.noreply.github.com>
2022-10-01 10:46:34 +0900

    Update footer.js

--- common/footer.js
+++ common/footer.js
@@ -1,15 +1,15 @@
   var mybutton = document.getElementById("topBtn");
 
 
-function topFunction() {
-window.scrollTo({top:0, behavior:'smooth'});
-document.body.scrollTop = 0; // For Safari
-document.documentElement.scrollTop = 0; //}
 
-};
-
-
-
+function topFunction() {if (x.matches){
+		window.scrollTo({top:0, behavior:'smooth'});
+	} else {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;}
+}
+	 
+var x = window.matchMedia("(min-width: 700px)");
 
 const img = document.querySelector('img')
 img.ondragstart = () => {
