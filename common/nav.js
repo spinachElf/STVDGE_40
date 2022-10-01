522331cfe0c0606f29d278d44f4db4196c8a8bc9
spinachElf <70534964+spinachElf@users.noreply.github.com>
2022-10-01 11:23:40 +0900

    Update nav.js

--- common/nav.js
+++ common/nav.js
@@ -116,36 +116,17 @@ document.addEventListener("DOMContentLoaded", function () {
               document.querySelector(".menuWrap").classList.remove("on");
               document.getElementById("menuBtn").src = "./common/icon_mnav1.svg";
               document.querySelector("#menuBtn").classList.remove("btnRotate");
+              document.querySelector("#body").classList.remove("scrollLock");
               document.querySelector("#dimmed").remove();
             } else {
               //메뉴 slideIn
               document.querySelector(".menuWrap").classList.add("on");
               document.getElementById("menuBtn").src = "./common/icon_mnav2.svg";
               document.querySelector("#menuBtn").classList.add("btnRotate");
-
+              document.querySelector("#body").classList.add("scrollLock")
               let div = document.createElement("div");
                          div.id = "dimmed";
                          document.body.append(div);
-
-                        //페이지 스크롤 락  모바일 이벤트 차단
-                        document
-                         .querySelector("#dimmed")
-                         .addEventListener(
-                              "scroll touchmove touchend mousewheel",
-                              function (e) {
-                                      e.preventDefault();
-                                      e.stopPropagation();
-                                      return false;
-                            }
-                            );
-                         function scrollDisable(){
-    $('body').addClass('scrollDisable').on('scroll touchmove mousewheel', function(e){
-        e.preventDefault();
-    });
-}
-function scrollAble(){
-    $('body').removeClass('scrollDisable').off('scroll touchmove mousewheel');
-}
 
 
                        }
